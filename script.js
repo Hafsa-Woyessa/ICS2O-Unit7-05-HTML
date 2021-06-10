0f8557d971e499c4999aba46f5e7cc828149d3ce
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-10 14:35:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('button').addEventListener('click', workschool )
+let age = '12'
+let day = 'Sunday'
+// And is &&
+// OR is ||
+
+function workschool () {
+ // gets the age and day of the week and coverts it to an integer
+ age = document.getElementById('input-one').value
+ age = parseInt(age)
+
+ day = document.getElementById('input-two').value
+ 
+ // the first example checks if the users is a student and it's a weekday
+ if (( age < 18 ) && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
+    document.getElementById('answer').innerHTML = 'Time for school'
+ } else if ((age < 18 ) && (day === 'Saturday' || day === 'Sunday')) {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
+ } else if  ((age >= 18) && (day !== 'Saturday' && day!== 'Sunday')) {
+   document.getElementById('answer').innerHTML = 'Time for work'
+  } else if ((age >= 18) && (day === 'Saturday' || day === 'Sunday')) {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
+  }
+}
