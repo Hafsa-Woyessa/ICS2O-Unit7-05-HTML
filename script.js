292dcbcf1d5dda661f20f977f63ae9ab6d78579e
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-10 14:58:45 +0000

    line 15

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ let day = 'Sunday'
 // OR is ||
 
 function workschool () {
-   // gets the age and day of the week and coverts it to an integer
-   age = document.getElementById('input-one').value
-   age = parseInt(age)
+  // gets the age and day of the week and coverts it to an integer
+  age = document.getElementById('input-one').value
+  age = parseInt(age)
 
-   day = document.getElementById('input-two').value
- 
-   // the first example checks if the users is a student and it's a weekday
+  day = document.getElementById('input-two').value
+
+ // the first example checks if the users is a student and it's a weekday
     if ((age < 18 ) && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
-      document.getElementById('answer').innerHTML = 'Time for school'
- }  else if ((age < 18 ) && (day === 'Saturday' || day === 'Sunday')) {
-      documnet.getElementById('answer').innerHTML = 'Time to relax for the weekend'
- }  else if  ((age >= 18) && (day !== 'Saturday' && day !== 'Sunday')) {
-      document.getElementById('answer').innerHTML = 'Time for work'
+    document.getElementById('answer').innerHTML = 'Time for school'
+    }  else if ((age < 18 ) && (day === 'Saturday' || day === 'Sunday')) {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
+ }  else if ((age >= 18) && (day !== 'Saturday' && day !== 'Sunday')) {
+    document.getElementById('answer').innerHTML = 'Time for work'
   } else if ((age >= 18) && (day === 'Saturday' || day === 'Sunday')) {
     document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
   }
